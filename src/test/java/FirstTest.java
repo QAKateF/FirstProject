6915cf7ef113f25ab1f6c2176117dcbdda9ae92c
QAKateF <134771804+QAKateF@users.noreply.github.com>
2023-06-03 09:26:55 +0300

    01.06.2023

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+    WebDriver wd;
+    String browser = "Chrome";
+    @Test
+    public void searchWebDriverTest (){
+        WebDriverManager.chromedriver().setup();
+        wd = new ChromeDriver();
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.navigate().to("https://www.google.com/");
+        wd.findElement(By.name("q")).sendKeys("Webdriver" + Keys.ENTER);
+        System.out.println(wd.getCurrentUrl());
+        wd.navigate().to("https://www.saucedemo.com/");
+        wd.findElement(By.name("user-name")).sendKeys("standard_user" + Keys.ENTER);
+        wd.findElement(By.name("password")).sendKeys("secret_sauce" + Keys.ENTER);
+        wd.findElement(By.name("login-button")).click();
+        wd.findElements(By.xpath("//*[@class='inventory_item']")).size();
+    }
+}
